e728852cfca7ebc8fe1845472d096df7d67d9b9a
gdmec07150747 <371785738@qq.com>
2016-12-18 22:37:45 +0800

    hh

--- app/src/main/java/com/example/wzhgwc/menu/MainActivity.java
+++ app/src/main/java/com/example/wzhgwc/menu/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
     private TextView tv1;
     private int mcount;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
